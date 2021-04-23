e663fbe814878593a1fccfbf48340d29e4fb0c35
Brian Nunez <nunez27@Brians-MacBook-Pro.local>
2021-04-23 11:35:44 -0400

    working through readme and componets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Route, Link, Switch } from 'react-router-dom';
 
-const App = () => {
-  
-  
+
+const pizzaformat = {
+  name: '',
+  size: '',
+  topping1: true,
+  topping2: true,
+  special: ''
+}
+
+const pizza = []
+
+const App = (props) => {
+
+  const [initialPizzaForm, setInitialPizzaForm] = useState(pizzaformat);
+  const [order, setOrder] = useState(pizza);
+
+  const postNewOrder = newOrder =>{
+    axios
+    .post('https://reqres.in/api/orders', newOrder)
+    .then(res => {
+      setOrder([res.data, ...order]);
+      setInitialPizzaForm(pizzaformat);
+    })
+    .catch((err) =>{
+      return(err)
+    })
+  };
+
+  return (
+    <div className='App'>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div className='.App-header .App-link'>
+            <Link to='/'>Home</Link>
+            <Link to='/Help'>Help</Link>
+        </div>
+      </nav>
+    </div>
+  );
 };
 export default App;
